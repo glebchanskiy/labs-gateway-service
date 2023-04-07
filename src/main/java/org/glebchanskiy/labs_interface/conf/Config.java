372bcb502c809bd652ca3ce9e40c6ce322c6d0d0
Levkov Gleb <Glebchanskiy@yandex.by>
2023-04-07 21:26:19 +0300

    fix: move reastTemplate bean init to config class

--- src/main/java/org/glebchanskiy/labs_interface/conf/Config.java
+++ src/main/java/org/glebchanskiy/labs_interface/conf/Config.java
@@ -2,6 +2,7 @@ package org.glebchanskiy.labs_interface.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
@@ -16,5 +17,10 @@ public class Config {
         loggingFilter.setIncludeQueryString(true);
         loggingFilter.setMaxPayloadLength(64000);
         return loggingFilter;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
