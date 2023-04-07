2c0a526743df66a5421b40bd12db8ef7a04e8186
Levkov Gleb <Glebchanskiy@yandex.by>
2023-04-07 11:41:21 +0300

    update: added aois3 service support

--- src/main/java/org/glebchanskiy/labs_interface/service/impl/DispatchServiceImpl.java
+++ src/main/java/org/glebchanskiy/labs_interface/service/impl/DispatchServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.swing.*;
+
 @Service
 @AllArgsConstructor
 public class DispatchServiceImpl implements DispatchService {
@@ -21,6 +23,9 @@ public class DispatchServiceImpl implements DispatchService {
             case "pdnf-validator" -> {
                 return new Answer(toPdnfValidator(message).getText());
             }
+            case "logic" -> {
+                return new Answer(toLogic(message).getText());
+            }
             case "test1" -> {
                 return new Answer("test1 answer");
             }
@@ -29,6 +34,17 @@ public class DispatchServiceImpl implements DispatchService {
             }
         }
         throw new MessageRecipientNotFoundException("Message from [" + message.getFrom() + "]. Recipient not found.", message.getTo());
+    }
+
+    private Message toLogic(Message message) {
+        try {
+            return restTemplate.postForEntity(
+                    "http://localhost:8051/validate",
+                    message,
+                    Message.class).getBody();
+        } catch (RestClientException e) {
+            throw new ServiceNotResponseException("Service: " + message.getTo() + " not response.");
+        }
     }
 
     private Message toPdnfValidator(Message message) {
